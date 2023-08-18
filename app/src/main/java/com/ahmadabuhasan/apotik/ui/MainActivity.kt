16db58a1dca19f19c2bd3467a6cca29b6a19e796
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2023-08-19 06:33:25 +0700

    add feature logout app

--- app/src/main/java/com/ahmadabuhasan/apotik/ui/MainActivity.kt
+++ app/src/main/java/com/ahmadabuhasan/apotik/ui/MainActivity.kt
@@ -4,14 +4,20 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.ahmadabuhasan.apotik.R
 import com.ahmadabuhasan.apotik.adapter.AdapterObat
 import com.ahmadabuhasan.apotik.api.ApiConfig
 import com.ahmadabuhasan.apotik.databinding.ActivityMainBinding
 import com.ahmadabuhasan.apotik.modal.ListObat
 import com.ahmadabuhasan.apotik.modal.ResponseGetObat
 import com.ahmadabuhasan.apotik.utils.SessionManager
+import es.dmoral.toasty.Toasty
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -29,6 +35,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val actionBar = supportActionBar
+        actionBar?.title = "Data"
 
         sessionManager = SessionManager(this)
         token = sessionManager.getToken
@@ -70,5 +79,35 @@ class MainActivity : AppCompatActivity() {
     private fun toCreateData() {
         val i = Intent(this, CreateActivity::class.java)
         startActivity(i)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.option_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.logout -> dialogLogout()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun dialogLogout() {
+        AlertDialog.Builder(this)
+            .setTitle("Logout")
+            .setMessage("Are you sure?")
+            .setPositiveButton("Yes") { _, _ ->
+                sessionManager.clearData()
+                val i = Intent(this, LoginActivity::class.java)
+                i.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+                startActivity(i)
+                finish()
+                Toasty.success(this, "You have been Successfully logout", Toast.LENGTH_SHORT).show()
+            }
+            .setNegativeButton("No") { dialog, _ ->
+                dialog.dismiss()
+            }
+            .show()
     }
 }
